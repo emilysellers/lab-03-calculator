ec009e9e8fd4b0365d1feda472219e65fbd6cf02
Emily Sellers <emilymelindasellers@gmail.com>
2021-06-03 14:33:47 -0700

    working add function!

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const addResult = document.getElementById('add-result');
 
 addButton.addEventListener('click', () => {
   console.log('add button clicked');
-})
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
